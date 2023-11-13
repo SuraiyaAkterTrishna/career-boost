51a08c2b94ba40855f7c5447bdf19d4006cf2bab
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-11-14 01:09:28 +0600

    start career boost website

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <button className='btn btn-primary'>Click me</button>
+    </>
+  )
+}
+
+export default App
